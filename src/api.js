abc5971746b6272f3798c86e731b8d4d9333ef78
Imam Hanafi <52751983+imamhanafi1998@users.noreply.github.com>
2023-05-22 16:11:19 +0700

    Update api.js

--- src/api.js
+++ src/api.js
@@ -20,34 +20,65 @@ const Message = require('../models/message')
 // const reportRouter = require('../routers/report')
 // const userSavedPostRouter = require('../routers/userSavedPost')
 
-io.on('connection', (socket) => {
-    console.log(`⚡: ${socket.id} user just connected!`);
-    socket.on('disconnect', () => {
-      console.log('🔥: A user disconnected');
-    });
-});
+// V1
+// io.on('connection', (socket) => {
+//     console.log(`⚡: ${socket.id} user just connected!`);
+//     socket.on('disconnect', () => {
+//       console.log('🔥: A user disconnected');
+//     });
+// });
 
-router.get('/messages', async (req, res) => {	
-	Message.find({}, (err, messages) => {
-        res.send(messages);
-        console.log("sukses")
-    })
-})
+// router.get('/messages', async (req, res) => {	
+// 	Message.find({}, (err, messages) => {
+//         res.send(messages);
+//         console.log("sukses")
+//     })
+// })
 
-router.post('/messages', async (req, res) => {
-    const message = new Message(req.body);
-    await message.save((err) => {
-        if (err) {
-            console.log("gagal")
-            sendStatus(500);
-        }
-        io.emit('message', req.body)
-        res.sendStatus(200);
-    })
-})
+// router.post('/messages', async (req, res) => {
+//     const message = new Message(req.body);
+//     await message.save((err) => {
+//         if (err) {
+//             console.log("gagal")
+//             sendStatus(500);
+//         }
+//         io.emit('message', req.body)
+//         res.sendStatus(200);
+//     })
+// })
 
 app.use(express.json())
 app.use(cors())
+
+// V2
+let users = []
+
+io.on('connection', (socket) => {
+    console.log(`⚡: ${socket.id} user just connected!`)  
+    socket.on("message", data => {
+      io.emit("messageResponse", data)
+    })
+
+    socket.on("typing", data => (
+      socket.broadcast.emit("typingResponse", data)
+    ))
+
+    socket.on("newUser", data => {
+      users.push(data)
+      io.emit("newUserResponse", users)
+    })
+ 
+    socket.on('disconnect', () => {
+      console.log('🔥: A user disconnected');
+      users = users.filter(user => user.socketID !== socket.id)
+      io.emit("newUserResponse", users)
+      socket.disconnect()
+    });
+});
+
+app.get("/api", (req, res) => {
+  res.json({message: "Hello"})
+});
 
 app.use(`/.netlify/functions/api`, router)
 // app.use(`/.netlify/functions/api`, postRouter)
