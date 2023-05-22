b895aed2a06771723f5a6706ac45966817e569db
Imam Hanafi <52751983+imamhanafi1998@users.noreply.github.com>
2023-05-22 14:45:59 +0700

    UPDATE

---
+++ models/message.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+const Report = require('./report')
+
+const messageSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    message: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const Message = mongoose.model('Message', messageSchema)
+
+module.exports = Message
--- src/api.js
+++ src/api.js
@@ -3,22 +3,44 @@ const serverless = require("serverless-http");
 require('../db/mongoose')
 const cors = require('cors')
 const app = express()
+const router = express.Router()
 const port = 3000
 
-const userRouter = require('../routers/user')
-const postRouter = require('../routers/post')
-const commentRouter = require('../routers/comment')
-const reportRouter = require('../routers/report')
-const userSavedPostRouter = require('../routers/userSavedPost')
+const Message = require('../models/message')
+
+// const userRouter = require('../routers/user')
+// const postRouter = require('../routers/post')
+// const commentRouter = require('../routers/comment')
+// const reportRouter = require('../routers/report')
+// const userSavedPostRouter = require('../routers/userSavedPost')
+
+router.get('/messages', async (req, res) => {	
+	Message.find({}, (err, messages) => {
+        res.send(messages);
+        console.log("sukses")
+    })
+})
+
+router.post('/messages', async (req, res) => {
+    const message = new Message(req.body);
+    await message.save((err) => {
+        if (err) {
+            console.log("gagal")
+            sendStatus(500);
+        }
+        // io.emit('message', req.body)
+        res.sendStatus(200);
+    })
+})
 
 app.use(express.json())
 app.use(cors())
 
-app.use(`/.netlify/functions/api`, userRouter)
-app.use(`/.netlify/functions/api`, postRouter)
-app.use(`/.netlify/functions/api`, commentRouter)
-app.use(`/.netlify/functions/api`, reportRouter)
-app.use(`/.netlify/functions/api`, userSavedPostRouter)
+app.use(`/.netlify/functions/api`, router)
+// app.use(`/.netlify/functions/api`, postRouter)
+// app.use(`/.netlify/functions/api`, commentRouter)
+// app.use(`/.netlify/functions/api`, reportRouter)
+// app.use(`/.netlify/functions/api`, userSavedPostRouter)
 
 module.exports = app;
 module.exports.handler = serverless(app);
